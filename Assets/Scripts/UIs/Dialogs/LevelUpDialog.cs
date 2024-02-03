4124e5cd86b991e4c56e468d6154ea39c254b77e
parkrye <parkrye@gmail.com>
2024-02-03 17:06:07 +0900

    재화 수정

--- Assets/Scripts/UIs/Dialogs/LevelUpDialog.cs
+++ Assets/Scripts/UIs/Dialogs/LevelUpDialog.cs
@@ -24,28 +24,28 @@ public class LevelUpDialog : Dialog
         var instant = Instantiate(statSlotTemplate, content);
         instant.GetText("StatName").text = "HP";
         instant.GetText("StatDescription").text = $"{userData.ActorData.Hp}";
-        instant.GetText("StatUpCost").text = $"Ruby {userData.ActorData.Hp * 5}";
+        instant.GetText("StatUpCost").text = $"Diamond {userData.ActorData.Hp * 5}";
         _templates.Add("HP", instant);
         instant.GetButton("StatUpButton").onClick.AddListener(() => OnClickLevelUpButton("HP"));
 
         instant = Instantiate(statSlotTemplate, content);
         instant.GetText("StatName").text = "Move Speed";
         instant.GetText("StatDescription").text = $"{userData.ActorData.MoveSpeed}";
-        instant.GetText("StatUpCost").text = $"Ruby {userData.ActorData.MoveSpeed * 5}";
+        instant.GetText("StatUpCost").text = $"Diamond {userData.ActorData.MoveSpeed * 5}";
         _templates.Add("MS", instant);
         instant.GetButton("StatUpButton").onClick.AddListener(() => OnClickLevelUpButton("MS"));
 
         instant = Instantiate(statSlotTemplate, content);
         instant.GetText("StatName").text = "Attack Damage";
         instant.GetText("StatDescription").text = $"{userData.ActorData.AttackDamage}";
-        instant.GetText("StatUpCost").text = $"Ruby {userData.ActorData.AttackDamage * 5}";
+        instant.GetText("StatUpCost").text = $"Diamond {userData.ActorData.AttackDamage * 5}";
         _templates.Add("AD", instant);
         instant.GetButton("StatUpButton").onClick.AddListener(() => OnClickLevelUpButton("AD"));
 
         instant = Instantiate(statSlotTemplate, content);
         instant.GetText("StatName").text = "Attack Speed";
         instant.GetText("StatDescription").text = $"{userData.ActorData.AttackSpeed}";
-        instant.GetText("StatUpCost").text = $"Ruby {userData.ActorData.AttackSpeed * 5}";
+        instant.GetText("StatUpCost").text = $"Diamond {userData.ActorData.AttackSpeed * 5}";
         _templates.Add("AS", instant);
         instant.GetButton("StatUpButton").onClick.AddListener(() => OnClickLevelUpButton("AS"));
 
@@ -90,10 +90,10 @@ public class LevelUpDialog : Dialog
                 break;
             default:
                 _templates["LV"].GetImage("LevelBar").fillAmount = (float)userData.GetCurrency(CurrencyType.Exp) / (userData.ActorData.Level * 10);
-                _templates["HP"].GetText("StatUpCost").text = $"Ruby {userData.ActorData.Hp * 5}";
-                _templates["MS"].GetText("StatUpCost").text = $"Ruby {userData.ActorData.MoveSpeed * 5}";
-                _templates["AD"].GetText("StatUpCost").text = $"Ruby {userData.ActorData.AttackDamage * 5}";
-                _templates["AS"].GetText("StatUpCost").text = $"Ruby {userData.ActorData.AttackSpeed * 5}";
+                _templates["HP"].GetText("StatUpCost").text = $"Diamond {userData.ActorData.Hp * 5}";
+                _templates["MS"].GetText("StatUpCost").text = $"Diamond {userData.ActorData.MoveSpeed * 5}";
+                _templates["AD"].GetText("StatUpCost").text = $"Diamond {userData.ActorData.AttackDamage * 5}";
+                _templates["AS"].GetText("StatUpCost").text = $"Diamond {userData.ActorData.AttackSpeed * 5}";
                 break;
         }
     }
@@ -114,28 +114,28 @@ public class LevelUpDialog : Dialog
                 }
                 break;
             case "HP":
-                if (userData.TryUseCurrency(CurrencyType.Ruby, userData.ActorData.Hp * 5) == true)
+                if (userData.TryUseCurrency(CurrencyType.Diamond, userData.ActorData.Hp * 5) == true)
                 {
                     userData.ActorData.Hp += 1;
                     UpdateCost(target);
                 }
                 break;
             case "MS":
-                if (userData.TryUseCurrency(CurrencyType.Ruby, userData.ActorData.MoveSpeed * 5) == true)
+                if (userData.TryUseCurrency(CurrencyType.Diamond, userData.ActorData.MoveSpeed * 5) == true)
                 {
                     userData.ActorData.MoveSpeed += 1;
                     UpdateCost(target);
                 }
                 break;
             case "AD":
-                if (userData.TryUseCurrency(CurrencyType.Ruby, userData.ActorData.AttackDamage * 5) == true)
+                if (userData.TryUseCurrency(CurrencyType.Diamond, userData.ActorData.AttackDamage * 5) == true)
                 {
                     userData.ActorData.AttackDamage += 1;
                     UpdateCost(target);
                 }
                 break;
             case "AS":
-                if (userData.TryUseCurrency(CurrencyType.Ruby, userData.ActorData.AttackSpeed * 5) == true)
+                if (userData.TryUseCurrency(CurrencyType.Diamond, userData.ActorData.AttackSpeed * 5) == true)
                 {
                     userData.ActorData.AttackSpeed += 1;
                     UpdateCost(target);
