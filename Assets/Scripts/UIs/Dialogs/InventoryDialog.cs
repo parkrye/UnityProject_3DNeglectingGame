0e86337162f0a522c82d1208048b74ec4f412437
parkrye <parkrye@gmail.com>
2024-02-08 23:32:55 +0900

    인벤토리 UI 추가

--- Assets/Scripts/UIs/Dialogs/InventoryDialog.cs
+++ Assets/Scripts/UIs/Dialogs/InventoryDialog.cs
@@ -1,162 +1,180 @@
 using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
 using UnityEngine.Events;
 
 public class InventoryDialog : Dialog
 {
     private Dictionary<string, Template> _templates = new Dictionary<string, Template>();
-private UnityAction<CurrencyData> _updateCurrencyAction;
+    private UnityAction<CurrencyData> _updateCurrencyAction;
 
-public override void Init()
-{
-    base.Init();
+    private int _selectIndex = 0;
+    private Dictionary<int, Template> _inventorySlots = new Dictionary<int, Template>();
 
-    var userData = G.Data.User;
-
-    var levelSlotTemplate = GetTemplate("LevelFrameTemplate");
-    levelSlotTemplate.GetText("LevelText").text = $"Lv {userData.ActorData.Level}";
-    levelSlotTemplate.GetImage("LevelBar").fillAmount = (float)userData.GetCurrency(CurrencyType.Exp) / (userData.ActorData.Level * 10);
-    _templates.Add("LV", levelSlotTemplate);
-    levelSlotTemplate.GetButton("LevelUpButton").onClick.AddListener(() => OnClickLevelUpButton("LV"));
-
-    var statSlotTemplate = GetTemplate("StatFrameTemplate");
-    var content = GetContent("StatScroll");
-
-    var instant = Instantiate(statSlotTemplate, content);
-    instant.GetText("StatName").text = "HP";
-    instant.GetText("StatDescription").text = $"{userData.ActorData.Hp}";
-    instant.GetText("StatUpCost").text = $"Diamond {userData.ActorData.Hp * 5}";
-    _templates.Add("HP", instant);
-    instant.GetButton("StatUpButton").onClick.AddListener(() => OnClickLevelUpButton("HP"));
-
-    instant = Instantiate(statSlotTemplate, content);
-    instant.GetText("StatName").text = "Move Speed";
-    instant.GetText("StatDescription").text = $"{userData.ActorData.MoveSpeed}";
-    instant.GetText("StatUpCost").text = $"Diamond {userData.ActorData.MoveSpeed * 5}";
-    _templates.Add("MS", instant);
-    instant.GetButton("StatUpButton").onClick.AddListener(() => OnClickLevelUpButton("MS"));
-
-    instant = Instantiate(statSlotTemplate, content);
-    instant.GetText("StatName").text = "Attack Damage";
-    instant.GetText("StatDescription").text = $"{userData.ActorData.AttackDamage}";
-    instant.GetText("StatUpCost").text = $"Diamond {userData.ActorData.AttackDamage * 5}";
-    _templates.Add("AD", instant);
-    instant.GetButton("StatUpButton").onClick.AddListener(() => OnClickLevelUpButton("AD"));
-
-    instant = Instantiate(statSlotTemplate, content);
-    instant.GetText("StatName").text = "Attack Speed";
-    instant.GetText("StatDescription").text = $"{userData.ActorData.AttackSpeed}";
-    instant.GetText("StatUpCost").text = $"Diamond {userData.ActorData.AttackSpeed * 5}";
-    _templates.Add("AS", instant);
-    instant.GetButton("StatUpButton").onClick.AddListener(() => OnClickLevelUpButton("AS"));
-
-    statSlotTemplate.gameObject.SetActive(false);
-
-    _updateCurrencyAction = UpdateCurrency;
-}
-
-public override void OpenDialog()
-{
-    base.OpenDialog();
-    UpdateCost();
-    G.Data.User.CurrencyUpdate.AddListener(_updateCurrencyAction);
-}
-
-public override void CloseDialog()
-{
-    base.CloseDialog();
-    G.Data.User.CurrencyUpdate.RemoveListener(_updateCurrencyAction);
-}
-
-private void UpdateCost(string target = "")
-{
-    var userData = G.Data.User;
-    switch (target)
+    public override void Init()
     {
-        case "LV":
-            _templates[target].GetText("LevelText").text = $"Lv {userData.ActorData.Level}";
-            _templates[target].GetImage("LevelBar").fillAmount = (float)userData.GetCurrency(CurrencyType.Exp) / (userData.ActorData.Level * 10);
-            break;
-        case "HP":
-            _templates[target].GetText("StatUpCost").text = $"Diamond {userData.ActorData.Hp * 5}";
-            break;
-        case "MS":
-            _templates[target].GetText("StatUpCost").text = $"Diamond {userData.ActorData.MoveSpeed * 5}";
-            break;
-        case "AD":
-            _templates[target].GetText("StatUpCost").text = $"Diamond {userData.ActorData.AttackDamage * 5}";
-            break;
-        case "AS":
-            _templates[target].GetText("StatUpCost").text = $"Diamond {userData.ActorData.AttackSpeed * 5}";
-            break;
-        default:
-            _templates["LV"].GetImage("LevelBar").fillAmount = (float)userData.GetCurrency(CurrencyType.Exp) / (userData.ActorData.Level * 10);
-            _templates["HP"].GetText("StatUpCost").text = $"Diamond {userData.ActorData.Hp * 5}";
-            _templates["MS"].GetText("StatUpCost").text = $"Diamond {userData.ActorData.MoveSpeed * 5}";
-            _templates["AD"].GetText("StatUpCost").text = $"Diamond {userData.ActorData.AttackDamage * 5}";
-            _templates["AS"].GetText("StatUpCost").text = $"Diamond {userData.ActorData.AttackSpeed * 5}";
-            break;
+        base.Init();
+
+        var equipmentDatas = G.Data.Equipment;
+        var itemTemplate = GetTemplate("ItemTemplate");
+        var content = GetContent("IventoryScroll");
+        foreach (var equipmentId in equipmentDatas.EquipmentData.Keys)
+        {
+            var instant = Instantiate(itemTemplate, content);
+            var equipment = equipmentDatas.GetEquipmentData(equipmentId);
+            instant.GetText("ItemName").text = equipment.Name;
+            instant.GetText("ItemLevel").text = equipment.Level.ToString();
+            instant.GetButton("ItemButton").onClick.AddListener(() => OnItemSlotClick(equipment.Id));
+            _inventorySlots.Add(equipment.Id, instant);
+        }
+        itemTemplate.gameObject.SetActive(false);
+
+        var equipButton = GetButton("EquipButton");
+        equipButton.onClick.AddListener(OnItemEquipClick);
+        var enhanceButton = GetButton("EnhanceButton");
+        enhanceButton.onClick.AddListener(OnItemEnhanceClick);
     }
-}
 
-private void OnClickLevelUpButton(string target)
-{
-    var userData = G.Data.User;
-
-    switch (target)
+    public override void OpenDialog()
     {
-        default:
-            break;
-        case "LV":
-            if (userData.TryUseCurrency(CurrencyType.Exp, userData.ActorData.Level * 10) == true)
-            {
-                userData.ActorData.Level += 1;
-                UpdateCost(target);
-            }
-            break;
-        case "HP":
-            if (userData.TryUseCurrency(CurrencyType.Diamond, userData.ActorData.Hp * 5) == true)
-            {
-                userData.ActorData.Hp += 1;
-                UpdateCost(target);
-            }
-            break;
-        case "MS":
-            if (userData.TryUseCurrency(CurrencyType.Diamond, userData.ActorData.MoveSpeed * 5) == true)
-            {
-                userData.ActorData.MoveSpeed += 1;
-                UpdateCost(target);
-            }
-            break;
-        case "AD":
-            if (userData.TryUseCurrency(CurrencyType.Diamond, userData.ActorData.AttackDamage * 5) == true)
-            {
-                userData.ActorData.AttackDamage += 1;
-                UpdateCost(target);
-            }
-            break;
-        case "AS":
-            if (userData.TryUseCurrency(CurrencyType.Diamond, userData.ActorData.AttackSpeed * 5) == true)
-            {
-                userData.ActorData.AttackSpeed += 1;
-                UpdateCost(target);
-            }
-            break;
-    }
-}
+        base.OpenDialog();
 
-private void UpdateCurrency(CurrencyData currencyData)
-{
-    switch (currencyData.Id)
-    {
-        case (int)CurrencyType.Gold:
-            break;
-        case (int)CurrencyType.Diamond:
-            break;
-        case (int)CurrencyType.Ruby:
-            break;
-        case (int)CurrencyType.Exp:
-            UpdateCost("LV");
-            break;
+        UpdateEquipItem();
+        UpdateSelectItem();
+        UpdateInventory();
     }
-}
+
+    public override void CloseDialog()
+    {
+        base.CloseDialog();
+    }
+
+    private void UpdateEquipItem()
+    {
+        var playerData = G.Data.User.PlayerData;
+
+        var weapon = playerData.Weapon;
+        var weaponTemplate = GetTemplate("Weapon");
+        if (weapon != null)
+        {
+            weaponTemplate.GetText("ItemName").text = weapon.Name;
+            weaponTemplate.GetText("ItemLevel").text = weapon.Level.ToString();
+            weaponTemplate.GetButton("ItemButton").onClick.AddListener(() => OnItemSlotClick(weapon.Id));
+        }
+        else
+        {
+            weaponTemplate.gameObject.SetActive(false);
+        }
+        var armor = playerData.Armor;
+        var armorTemplate = GetTemplate("Armor");
+        if (armor != null)
+        {
+            armorTemplate.GetText("ItemName").text = armor.Name;
+            armorTemplate.GetText("ItemLevel").text = armor.Level.ToString();
+            weaponTemplate.GetButton("ItemButton").onClick.AddListener(() => OnItemSlotClick(armor.Id));
+        }
+        else
+        {
+            armorTemplate.gameObject.SetActive(false);
+        }
+        var accessory = playerData.Armor;
+        var accessoryTemplate = GetTemplate("Accessory");
+        if (accessory != null)
+        {
+            accessoryTemplate.GetText("ItemName").text = accessory.Name;
+            accessoryTemplate.GetText("ItemLevel").text = accessory.Level.ToString();
+            weaponTemplate.GetButton("ItemButton").onClick.AddListener(() => OnItemSlotClick(accessory.Id));
+        }
+        else
+        {
+            accessoryTemplate.gameObject.SetActive(false);
+        }
+    }
+
+    private void UpdateSelectItem()
+    {
+        var selected = GetTemplate("Select");
+        var firstItem = G.Data.Equipment.GetEquipmentData(_selectIndex);
+        if (firstItem == null)
+            return;
+
+        selected.GetText("ItemName").text = firstItem.Name;
+        selected.GetText("ItemLevel").text = firstItem.Level.ToString();
+        selected.GetText("ItemType").text = ((EquipmentType)firstItem.Type).ToString();
+        selected.GetText("ItemValue").text = firstItem.Value.ToString();
+    }
+
+    private void UpdateInventory(int id = -1)
+    {
+        if(id < 0)
+        {
+            foreach(var slotId in _inventorySlots.Keys)
+            {
+                if (_inventorySlots.TryGetValue(slotId, out var slot) == false)
+                    continue;
+                var equipment = G.Data.Equipment.GetEquipmentData(slotId);
+                if (equipment == null)
+                    continue;
+
+                slot.GetText("ItemName").text = equipment.Name;
+                slot.GetText("ItemLevel").text = equipment.Level.ToString();
+            }
+        }
+        else
+        {
+            if (_inventorySlots.TryGetValue(id, out var slot) == false)
+                return;
+            var equipment = G.Data.Equipment.GetEquipmentData(id);
+            if (equipment == null) 
+                return;
+
+            slot.GetText("ItemName").text = equipment.Name;
+            slot.GetText("ItemLevel").text = equipment.Level.ToString();
+        }
+    }
+
+    private void OnItemSlotClick(int id)
+    {
+        if (_selectIndex == id)
+            return;
+
+        _selectIndex = id;
+
+        UpdateSelectItem();
+    }
+
+    private void OnItemEquipClick()
+    {
+        var equipment = G.Data.Equipment.GetEquipmentData(_selectIndex);
+        if (equipment == null)
+            return;
+        if (equipment.Level == 0)
+            return;
+
+        switch ((EquipmentType)equipment.Type)
+        {
+            case EquipmentType.Weapon:
+                G.Data.User.PlayerData.Weapon = equipment;
+                break;
+            case EquipmentType.Armor:
+                G.Data.User.PlayerData.Armor = equipment;
+                break;
+            case EquipmentType.Accessory:
+                G.Data.User.PlayerData.Accessory = equipment;
+                break;
+        }
+    }
+
+    private void OnItemEnhanceClick()
+    {
+        var equipment = G.Data.Equipment.GetEquipmentData(_selectIndex);
+        if (equipment == null)
+            return;
+        if (equipment.Level == 0)
+            return;
+
+        if (G.Data.User.TryUseCurrency(CurrencyType.Ruby, (equipment.Level) * 10))
+        {
+            equipment.Level++;
+        }
+    }
 }
